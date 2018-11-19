44bf0e1cecb1d13b7924902d0435a2a743299a6e
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-11-19 14:22:25 +0300

    Update receiver chatroom new message count

--- functions/index.js
+++ functions/index.js
@@ -4,10 +4,10 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// Send new chat message notification.
-// Listen for new chat messages added to /chatrooms/:chatroomId/messages/:messageId
+// Listen for new chat messages added to /chatrooms/:chatroomId/messages/:messageId ,
+// update corresponding chatroom of the sender and the receiver,
 // and send data FCM message to the receiver of the new chat message.
-exports.sendNewChatMessageNotification = functions.firestore.document('/chatrooms/{chatroomUid}/messages/{messageUid}')
+exports.onNewChatMessage = functions.firestore.document('/chatrooms/{chatroomUid}/messages/{messageUid}')
 	// This is triggered on new document creation
     .onCreate((snap, context) => {
     	// Get chat message from the document
@@ -19,6 +19,10 @@ exports.sendNewChatMessageNotification = functions.firestore.document('/chatroom
       	const messageText = message.message_text;
 
         let senderName;
+        let receiverName;
+        let receiverToken;
+
+		let chatroomUid;
 
 		// Get sender user
 		// (in return statement, because this method must return promise)
@@ -43,14 +47,56 @@ exports.sendNewChatMessageNotification = functions.firestore.document('/chatroom
 
 		      	// Get receiver user
 		      	// (in return statement, because this method must return promise)
-		  		return admin.firestore().collection('users').doc(receiverUid).get()
+		  		return admin.firestore().collection('users').doc(receiverUid).get();
 		    })
             .then(doc => {
             	// Get receiver user from the document
  	   	        const receiver = doc.data();
 
+      			// Get receiver user name and username
+		        const name = receiver.name;
+		        const userName = receiver.username;
+
+		        // Init receiver name with username or name
+		        if (userName !== "") {
+		        	receiverName = userName;
+		        } else {
+		        	receiverName = name;
+		        }
+
 				// Get receiver user's FCM token		        	
-		        const token = receiver.fcm_token;
+		        receiverToken = receiver.fcm_token;
+
+		        // Create chatroom UID
+		        if (senderUid < receiverUid) {
+		        	chatroomUid = `${senderUid}_${receiverUid}`;
+		        } else {
+		        	chatroomUid = `${receiverUid}_${senderUid}`;
+		        }
+
+		        console.log('Chatroom uid = ', chatroomUid); 
+
+		        // Get receiver's chatroom
+		      	// (in return statement, because this method must return promise)
+		  		return admin.firestore().collection('userChatrooms').doc(receiverUid).collection('chatroomsOfUser').doc(chatroomUid).get();
+            })
+			.then(doc => {
+				// Get chatroom from the document
+				const chatroom = doc.data();
+
+				console.log('Chatroom = ', chatroom); 
+
+				// Get current new message count
+				const newMessageCount = chatroom.newMessageCount;
+
+				// Increment new message count by 1
+				const incrementedNewMessageCount = newMessageCount + 1;
+
+				// Update new message counter in receiver's chatroom
+				return doc.ref.set({newMessageCount: incrementedNewMessageCount}, {merge: true});
+			})
+			.then(response => {
+				console.log('response = ', response); 
 
 		        // Create FCM message with sender uid and name and message text.
 		        // We must send DATA FCM message, not notification message
@@ -69,6 +115,6 @@ exports.sendNewChatMessageNotification = functions.firestore.document('/chatroom
 
 		        // Send FCM message to the device with specified FCM token.
 		        // (again in return statement, because this method must return promise)
-		        return admin.messaging().sendToDevice(token, payload);
+		        return admin.messaging().sendToDevice(receiverToken, payload);
             });
     });
